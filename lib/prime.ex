76f6551998c0b4882e0f9e34bdea3016c1282e90
Vince Foley <vince@newrelic.com>
2015-03-05 10:14:12 -0800

    Enable skipping even numbers

--- lib/prime.ex
+++ lib/prime.ex
@@ -1,25 +1,28 @@
 defmodule Prime do
 
 
-  def nth_prime(n) do
-    primes(n, 1, [])
-  end
+  def nth_prime(1), do: 2
+  def nth_prime(2), do: 3
+  def nth_prime(n), do: primes(n, 3, 1)
 
-  def primes(n, curr, acc) do
+  def primes(n, curr, size) do
 
-    if is_prime?(curr), do: acc = [curr | acc]
+    if is_prime?(curr), do: size = size + 1
 
     cond do
-      length(acc) == n ->
+      size == n ->
         curr
       true ->
-        primes(n, curr + 1, acc)
+        primes(n, curr + 2, size)
     end
   end
 
   def is_prime?(1), do: false
   def is_prime?(2), do: true
   def is_prime?(3), do: true
-  def is_prime?(n), do: Enum.to_list(2..div(n,2)) |> Enum.all?( &(rem(n, &1) != 0) )
+  def is_prime?(n) do
+    Enum.to_list(2..div(n,2))
+      |> Enum.all?( &(rem(n, &1) != 0) )
+  end
 
 end
