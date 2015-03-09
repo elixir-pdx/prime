132a4de903c0a5d9c8d0bd5092fa3b6d594a5b38
Vince Foley <vince@newrelic.com>
2015-03-09 15:04:30 -0700

    Cleanup methods

--- lib/prime.ex
+++ lib/prime.ex
@@ -1,28 +1,23 @@
 defmodule Prime do
 
+  def nth_prime(1),   do: 2
+  def nth_prime(2),   do: 3
+  def nth_prime(nth), do: nth_prime(nth, 3, 2)
 
-  def nth_prime(1), do: 2
-  def nth_prime(2), do: 3
-  def nth_prime(n), do: primes(n, 3, 1)
-
-  def primes(n, curr, size) do
-
-    if is_prime?(curr), do: size = size + 1
-
-    cond do
-      size == n ->
-        curr
-      true ->
-        primes(n, curr + 2, size)
+  def nth_prime(nth, current, count) do
+    case is_prime?(current) do
+      true when count == nth ->
+        current
+      true  ->
+        nth_prime(nth, current + 2, count + 1)
+      false ->
+        nth_prime(nth, current + 2, count)
     end
   end
 
-  def is_prime?(1), do: false
-  def is_prime?(2), do: true
-  def is_prime?(3), do: true
-  def is_prime?(n) do
-    Enum.to_list(2..div(n,2))
-      |> Enum.all?( &(rem(n, &1) != 0) )
+  defp is_prime?(n) do
+    Enum.to_list(2..round(:math.sqrt(n)))
+      |> Enum.any?(fn (x) -> rem(n, x) == 0 end)
+      |> Kernel.not
   end
-
 end
--- test/prime_test.exs
+++ test/prime_test.exs
@@ -1,12 +1,6 @@
 defmodule PrimeTest do
   use ExUnit.Case
 
-  test "is_prime" do
-    assert Prime.is_prime?(9) == false
-    assert Prime.is_prime?(29) == true
-    assert Prime.is_prime?(30) == false
-  end
-
   test "n_prime 1" do
     assert Prime.nth_prime(1) == 2
   end
@@ -25,5 +19,9 @@ defmodule PrimeTest do
 
   test "n_prime 1000" do
     assert Prime.nth_prime(1000) == 7919
+  end
+
+  test "n_prime 5000" do
+    assert Prime.nth_prime(5000) == 48611
   end
 end
